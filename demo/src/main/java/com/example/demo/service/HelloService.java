cd64905c3fcd475d8d1c853c8b88e2ecbbc91ed2
hyune7142 <hyune7142@daum.net>
2019-07-10 10:40:39 +0900

    DB connect - ver0.1

--- demo/src/main/java/com/example/demo/service/HelloService.java
+++ demo/src/main/java/com/example/demo/service/HelloService.java
@@ -2,6 +2,9 @@ package com.example.demo.service;
 
 import java.util.Map;
 
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
 public interface HelloService {
 	
 	public Map<String, String> getMetaData(String url);
--- demo/src/main/java/com/example/demo/service/HelloServiceImpl.java
+++ demo/src/main/java/com/example/demo/service/HelloServiceImpl.java
@@ -19,12 +19,12 @@ public class HelloServiceImpl implements HelloService {
 		try {
 			
 			String connUrl = "https://"+url;
-			System.out.println("URL"+connUrl);
 			Document doc = Jsoup.connect(connUrl).get();
-			System.out.println("독독"+doc);
 			String title;
 			String description;
 			String image ;
+			
+			
 			Elements metaTitle = doc.getElementsByAttributeValue("property", "og:title");		
 			if(metaTitle.attr("content").equals("")) {
 				title=doc.select("title").text();
@@ -32,30 +32,22 @@ public class HelloServiceImpl implements HelloService {
 				 title=metaTitle.attr("content");
 			};
 			
-			
 			Elements metaDescription = doc.getElementsByAttributeValue("property", "og:description");
 			if(metaDescription.attr("content").equals("")) {
 				description =  doc.getElementsByAttributeValue("name", "description").attr("content");
+				if(description.equals("")) {
+					description = "등록된 설명이 없습니다.";
+				}
 			}else {
 				description	 = metaDescription.attr("content");
 			}
 			
-			
 			Elements metaImage = doc.getElementsByAttributeValue("property", "og:image");			
-			
 			if(metaImage.attr("content").equals("")) {
 				image="https://user-images.githubusercontent.com/24848110/33519396-7e56363c-d79d-11e7-969b-09782f5ccbab.png";
-					
-					
 			}else {
 				image= metaImage.attr("content");
 			}
-		
-			
-			
-//			System.out.println(title);
-//			System.out.println(description);
-//			System.out.println(image);
 			
 			result.put("title", title);
 			result.put("description", description);
